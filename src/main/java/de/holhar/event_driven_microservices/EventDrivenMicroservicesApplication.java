f6dbd5d42e8002ac9fa903bb3803582052e2cb38
Holger Harms <holger.harms@adesso.de>
2022-06-13 17:35:20 +0200

    initial commit

---
+++ src/main/java/de/holhar/event_driven_microservices/EventDrivenMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package de.holhar.event_driven_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenMicroservicesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EventDrivenMicroservicesApplication.class, args);
+  }
+
+}
